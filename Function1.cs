8b3f100ea0e280a3c2bbe62bd465da36a6c13066
praat <praat@PAULSPLACE>
2024-02-20 23:23:07 -0500

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string n1 = req.Query["n1"];
+            string n2 = req.Query["n2"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+            int num1 = Convert.ToInt32(n1);
+            int num2 = Convert.ToInt32(n2);
+            int rval = num1 + num2;
+
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}{friend} This HTTP triggered function executed successfully.";
+
+
+            //return new OkObjectResult(responseMessage);
+            return new OkObjectResult(Convert.ToString(rval));
+        }
+    }
+}
