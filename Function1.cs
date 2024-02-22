5c350a3e34fa288b24640b4609b776bf809ad962
@PaulD12345 <praathome@yahoo.com>
2024-02-22 02:27:03 -0500

    20

--- Function1.cs
+++ Function1.cs
@@ -20,14 +20,15 @@ namespace AzureFunctionApp1
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string n1 = req.Query["n1"];
-            string n2 = req.Query["n2"];    // New comment22
+            string n2 = req.Query["n2"];    // New comment33
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             //name = name ?? data?.name;
             int num1 = Convert.ToInt32(n1);
             int num2 = Convert.ToInt32(n2);
-            int rval = num1 + num2;
+
+            int rval = (num1 + num2) * 2;
 
             //string responseMessage = string.IsNullOrEmpty(name)
             //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
